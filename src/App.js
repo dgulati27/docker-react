12c5026d07bf7e619cb9850df1f1fdf39abf4fe8
unknown <dimpygulati0@gmail.com>
2020-08-18 13:15:03 +0530

    just tested app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          YippY! Dimpy did this!
+          YippY! Dimpy did this on 18 August!
         </p>
         <a
           className="App-link"
